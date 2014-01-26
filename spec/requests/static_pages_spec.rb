a760ef398df2661ee804973657b149e83262f87c
jplindgren <jplindgrenbr@hotmail.com>
2014-01-26 19:34:54 -0200

    named routes and partial views

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,53 +3,34 @@ require 'spec_helper'
 #do not forget to add in spec_helper the capybara DSL
 describe "Static pages" do
 
-  let(:base_title) { 'MicroTwitter' } 
+  let(:base_title) { 'MicroTwitter ' } 
+  subject { page }
 
   describe "Home Page" do
-    it "should have content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
+    before { visit root_path }
 
-    it "should have the right title" do
-       visit '/static_pages/home'
-	     expect(page).to have_title("#{base_title}")
-	  end
+    it { should have_content('Welcome to the MicroTwitter') }
+    it { should have_title("#{base_title}")}
   end
 
   describe "Help page" do
-    it "should have content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+    before { visit help_path }
 
-    it "should have the right title" do
-      visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} | Help")
-	  end
+    it { should have_content('Help') }
+    it { should have_title("#{base_title} | Help") }
   end
 
   describe "About page" do
-    it "should have content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
+    before { visit about_path }
 
-    it "should have the right title" do
-      visit '/static_pages/about'
-	    expect(page).to have_title("#{base_title} | About us")
-    end
+    it { should have_content 'About Us'}
+    it { should have_title "#{base_title} | About us" }
   end
 
   describe "Contact page" do
-    it "Should have content 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_content("Contact")
-    end
+    before { visit contact_path }
 
-    it "Should have the right title" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title} | Contact")
-    end
+    it { should have_content 'Contact' }
+    it { should have_title "#{base_title} | Contact" }
   end
 end
