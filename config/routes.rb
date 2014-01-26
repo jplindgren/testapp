e67dfd07de5152d670a296b33145c8551e69a0d4
jplindgren <jplindgrenbr@hotmail.com>
2014-01-26 21:21:24 -0200

    Add named routes to signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ TestApp::Application.routes.draw do
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
+  match '/signup',  to: 'users#new', via: 'get'
   resources :microposts
   resources :users
 
