19ea4840eb0d9b78cef96aa8b0fe49d6ecbd7065
jplindgren <jplindgrenbr@hotmail.com>
2014-01-26 22:22:46 -0200

    rafactor some rpsec tests, create new rspec test to application helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,36 +1,51 @@
 require 'spec_helper'
 
+include ApplicationHelper
+
 #do not forget to add in spec_helper the capybara DSL
 describe "Static pages" do
-
-  let(:base_title) { 'MicroTwitter ' } 
   subject { page }
+  let(:base_title) { 'MicroTwitter ' } 
+  
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
   describe "Home Page" do
     before { visit root_path }
 
-    it { should have_content('Welcome to the MicroTwitter') }
-    it { should have_title("#{base_title}")}
+    let(:heading) { 'MicroTwitter' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home')}
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_content('Help') }
-    it { should have_title("#{base_title} | Help") }
+    let(:heading) { "Help" }
+    let(:page_title) { "Help" }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_content 'About Us'}
-    it { should have_title "#{base_title} | About us" }
+    let(:heading) { "About us" }
+    let(:page_title) { "About us" }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_content 'Contact' }
-    it { should have_title "#{base_title} | Contact" }
+    let(:heading) { "Contact" }
+    let(:page_title) { "Contact" }
+    
+    it_should_behave_like "all static pages"
   end
 end
