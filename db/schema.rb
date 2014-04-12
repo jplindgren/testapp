8cd2489318789beaea53f6c34d4178c7e0ceddc1
jplindgren <jplindgrenbr@hotmail.com>
2014-04-12 01:37:12 -0300

    Show micrposts

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(:version => 20140410025123) do
 
   add_index "microposts", ["user_id", "created_at"], :name => "index_microposts_on_user_id_and_created_at"
 
+  create_table "oferta", :force => true do |t|
+    t.string   "description"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.integer  "maximum_subscriptions"
+    t.string   "course_code"
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
+  end
+
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -48,9 +48,18 @@ describe "User pages" do
 
   describe "Profile page" do
   	let(:user) { FactoryGirl.create(:user) }
+    let!(:micropost1) { FactoryGirl.create(:micropost, user: user, content: "foo") }
+    let!(:micropost2) { FactoryGirl.create(:micropost, user: user, content: "bar") }
+
   	before { visit user_path(user) }
   	it { should have_content(user.name) }
   	it { should have_title(user.name) }
+
+    describe "microposts" do
+      it { should have_content(micropost1.content) }
+      it { should have_content(micropost2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
 
   describe "signup" do
